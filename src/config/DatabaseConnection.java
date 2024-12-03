e9665f5b1842bd0487302367a29f5fd692ffcdbb
Karunia Putri <148734328+kapuut@users.noreply.github.com>
2024-12-03 18:35:30 +0800

    F1D022010_Karunia Putri

---
+++ src/config/DatabaseConnection.java
@@ -0,0 +1,24 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/flappybird_db";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+    private static Connection connection = null;
+    
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            } catch (ClassNotFoundException e) {
+                throw new SQLException("MySQL JDBC Driver not found.", e);
+            }
+        }
+        return connection;
+    }
+}
